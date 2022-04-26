dea285ad39e4708ec31d82a0d82dc070f18e4da2
emeraldinc <87934913+emeraldinc@users.noreply.github.com>
2022-04-26 15:51:13 +0300

    Add files via upload

--- windowsize.js
+++ windowsize.js
@@ -1,6 +1,15 @@
-
+alert(navigator.platform);
 if (navigator.platform === "Linux armv8l") {
-    window.location.href = 'mobile.html';
+    
+	if(window.name === "About Emerald Inc."){window.location.href = 'about mobile.html';}
+	
+	if(window.name === "Contact Emerald Inc."){window.location.href = 'contact mobile.html';}
+	
+	if(window.name === "Apps for Windows"){window.location.href = 'android mobile.html';}
+	
+	if(window.name === "Apps for Android"){window.location.href = 'windows mobile.html';}
+	
+	if(window.name === "Other apps"){window.location.href = 'other mobile.html';}
+	
+	if(window.name === "Emerald Inc."){window.location.href = 'mobile.html';}
 }
-
-
