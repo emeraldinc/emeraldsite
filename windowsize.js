1eab684d639f25acc194eec42b409315c07786cb
emeraldinc <87934913+emeraldinc@users.noreply.github.com>
2022-04-26 20:45:57 +0300

    Add files via upload

--- windowsize.js
+++ windowsize.js
@@ -1,16 +1,15 @@
 
-
 if (navigator.platform === "Linux armv8l") {
     
-	if(window.name === "About Emerald Inc."){window.location.href = 'about mobile.html';}
+	if(window.name === "About Emerald Inc."){window.location.href = 'about mobile.html'; window.name = "About Emerald Inc. (m)";}
 	
-	if(window.name === "Contact Emerald Inc."){window.location.href = 'contact mobile.html';}
+	if(window.name === "Contact Emerald Inc."){window.location.href = 'contact mobile.html';window.name = "Contact Emerald Inc. (m)";}
 	
-	if(window.name === "Apps for Windows"){window.location.href = 'android mobile.html';}
+	if(window.name === "Apps for Windows"){window.location.href = 'android mobile.html';window.name = "Apps for Windows(m)";}
 	
-	if(window.name === "Apps for Android"){window.location.href = 'windows mobile.html';}
+	if(window.name === "Apps for Android"){window.location.href = 'windows mobile.html';window.name = "Apps for Android (m)";}
 	
-	if(window.name === "Other apps"){window.location.href = 'other mobile.html';}
+	if(window.name === "Other apps"){window.location.href = 'other mobile.html';window.name = "Other apps (m)";}
 	
-	if(window.name === "Emerald Inc."){window.location.href = 'mobile.html';}
+	if(window.name === "Emerald Inc."){window.location.href = 'mobile.html';window.name = "Emerald Inc. (m)";}
 }
