5b63a5367599ebbd80c71170cac27045cb741793
emeraldinc <87934913+emeraldinc@users.noreply.github.com>
2022-05-04 10:02:33 +0300

    Update windowsize.js

--- windowsize.js
+++ windowsize.js
@@ -1,13 +1,13 @@
 
-if (navigator.platform === "Linux armv8l") {
+if (navigator.platform === "Linux armv8l" || navigator.platform === "Iphone") {
     
 	if(window.name === "About Emerald Inc."){window.location.href = 'about mobile.html'; window.name = "About Emerald Inc. (m)";}
 	
 	if(window.name === "Contact Emerald Inc."){window.location.href = 'contact mobile.html';window.name = "Contact Emerald Inc. (m)";}
 	
-	if(window.name === "Apps for Windows"){window.location.href = 'android mobile.html';window.name = "Apps for Windows(m)";}
+	if(window.name === "Apps for Windows"){window.location.href = 'windows mobile.html';window.name = "Apps for Windows(m)";}
 	
-	if(window.name === "Apps for Android"){window.location.href = 'windows mobile.html';window.name = "Apps for Android (m)";}
+	if(window.name === "Apps for Android"){window.location.href = 'android mobile.html';window.name = "Apps for Android (m)";}
 	
 	if(window.name === "Other apps"){window.location.href = 'other mobile.html';window.name = "Other apps (m)";}
 	
